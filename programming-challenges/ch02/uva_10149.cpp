04f030e8995a6965a31c7ce70057e5614816f0e0
darrenhp <darrenhp@gamil.com>
2013-06-14 02:18:29 +0800

    refactor

--- programming-challenges/ch02/uva_10149.cpp
+++ programming-challenges/ch02/uva_10149.cpp
@@ -56,83 +56,86 @@ int cal_nbits(int n){
   return s;
 }
 
+int get_score(int a[5], int cat){
+  int score = 0;
+  switch(cat){
+  case 0:
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+    for (int i = 0; i < 5; i++){
+      if (a[i] == cat + 1){
+        score += cat + 1;
+      }
+    }
+    break;
+  case 6:
+    score = accumulate(a, a + 5, 0);
+    break;
+  case 7:
+    if (a[0] == a[2] || a[1] == a[3] || a[2] == a[4]){
+      score = accumulate(a, a + 5, 0);
+    }
+    break;
+  case 8:
+    if (a[0] == a[3] || a[1] == a[4]){
+      score = accumulate(a, a + 5, 0);
+    }
+    break;
+  case 9:
+    if (a[0] == a[4]){
+      score = 50;
+    }
+    break;
+  case 10:
+    if ((a[0] + 1 == a[1] && a[1] + 1 == a[2] && a[2] + 1 == a[3])
+      || (a[1] + 1 == a[2] && a[2] + 1 == a[3] && a[3] + 1 == a[4])){
+      score = 25;
+    }
+    break;
+  case 11:
+    if (a[0] + 1 == a[1] && a[1] + 1 == a[2] && a[2] + 1 == a[3] && a[3] + 1 == a[4]){
+      score = 35;
+    }
+    break;
+  case 12:
+    if ((a[0] == a[2] && a[3] == a[4]) || (a[0] == a[1] && a[2] == a[4])){
+      score = 40;
+    }
+    break;
+  }
+  return score;
+}
+
 void process(){
   for (int round = 0; round < 13; round++){
-    int (&a)[5] = dices[round];
-    for (int st1 = (1 << 13) - 1; st1 >= 0; st1--){
-      int nbits = cal_nbits(st1);
-      if (nbits != round){
-        continue;
-      }
-      for (int st2 = 63; st2 >=0; st2--){
-        int &s = dp[st1][st2];
-        if (s == -1){
+    for (int cat = 0; cat < 13; cat++){
+      const int score = get_score(dices[round], cat);
+      for (int st1 = (1 << 13) - 1; st1 >= 0; st1--){
+        if ( (st1 & (1 << cat)) != 0){
           continue;
         }
-        int score = 0;
-        for (int cat = 0; cat < 13; cat++){
-          if ((st1 & (1 << cat)) != 0){
+        if (cal_nbits(st1) != round){
+          continue;
+        }
+        for (int st2 = 63; st2 >=0; st2--){
+          int &s = dp[st1][st2];
+          if (s == -1){
             continue;
-          }
-          score = 0;
-          switch(cat){
-          case 0:
-          case 1:
-          case 2:
-          case 3:
-          case 4:
-          case 5:
-            for (int i = 0; i < 5; i++){
-              if (a[i] == cat + 1){
-                score += cat + 1;
-              }
-            }
-            break;
-          case 6:
-            score = accumulate(a, a + 5, 0);
-            break;
-          case 7:
-            if (a[0] == a[2] || a[1] == a[3] || a[2] == a[4]){
-              score = accumulate(a, a + 5, 0);
-            }
-            break;
-          case 8:
-            if (a[0] == a[3] || a[1] == a[4]){
-              score = accumulate(a, a + 5, 0);
-            }
-            break;
-          case 9:
-            if (a[0] == a[4]){
-              score = 50;
-            }
-            break;
-          case 10:
-            if ((a[0] + 1 == a[1] && a[1] + 1 == a[2] && a[2] + 1 == a[3])
-              || (a[1] + 1 == a[2] && a[2] + 1 == a[3] && a[3] + 1 == a[4])){
-              score = 25;
-            }
-            break;
-          case 11:
-            if (a[0] + 1 == a[1] && a[1] + 1 == a[2] && a[2] + 1 == a[3] && a[3] + 1 == a[4]){
-              score = 35;
-            }
-            break;
-          case 12:
-            if ((a[0] == a[2] && a[3] == a[4]) || (a[0] == a[1] && a[2] == a[4])){
-              score = 40;
-            }
-            break;
           }
           int next_st1 = st1 | (1 << cat);
           int next_st2 = st2 + (cat < 6 ? score : 0);
+          int bouns = 0;
           if (next_st2 >= 63 && cat < 6){
             next_st2 = 63;
             if (st2 < 63){
-              score += 35; 
+              bouns = 35;
             }
           }
-          if (s + score > dp[next_st1][next_st2]){
-            dp[next_st1][next_st2] = s + score;
+          if (s + score + bouns> dp[next_st1][next_st2]){
+            dp[next_st1][next_st2] = s + score + bouns;
             prev1[next_st1][next_st2] = st1;
             prev2[next_st1][next_st2] = st2;
             prev_round[next_st1][next_st2] = round;
