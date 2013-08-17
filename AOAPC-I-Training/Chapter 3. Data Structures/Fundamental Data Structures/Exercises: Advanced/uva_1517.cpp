a3f1db48874ad590ff8b33c46812e722492d305a
darrenhp <darrenhp@gamil.com>
2013-08-17 22:35:42 +0800

    Done

---
+++ AOAPC-I-Training/Chapter 3. Data Structures/Fundamental Data Structures/Exercises: Advanced/uva_1517.cpp
@@ -0,0 +1,321 @@
+//Clibrary:
+#include<cassert>
+#include<cctype>
+#include<cerrno>
+//#include<cfenv>
+#include<cfloat>
+//#include<cinttypes>
+#include<ciso646>
+#include<climits>
+#include<clocale>
+#include<cmath>
+#include<csetjmp>
+#include<csignal>
+#include<cstdarg>
+//#include<cstdbool>
+#include<cstddef>
+//#include<cstdint>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+//#include<ctgmath>
+#include<ctime>
+//#include<cuchar>
+#include<cwchar>
+#include<cwctype>
+//Containers:
+//#include<array>
+#include<bitset>
+#include<deque>
+//#include<forward_list>
+#include<list>
+#include<map>
+#include<queue>
+#include<set>
+#include<stack>
+//#include<unordered_map>
+//#include<unordered_set>
+#include<vector>
+//Input/Output:
+#include<fstream>
+#include<iomanip>
+#include<ios>
+#include<iosfwd>
+#include<iostream>
+#include<istream>
+#include<ostream>
+#include<sstream>
+#include<streambuf>
+//Other:
+#include<algorithm>
+//#include<chrono>
+//#include<codecvt>
+#include<complex>
+#include<exception>
+#include<functional>
+//#include<initializer_list>
+#include<iterator>
+#include<limits>
+#include<locale>
+#include<memory>
+#include<new>
+#include<numeric>
+//#include<random>
+//#include<ratio>
+//#include<regex>
+#include<stdexcept>
+#include<string>
+//#include<system_error>
+//#include<tuple>
+//#include<typeindex>
+#include<typeinfo>
+//#include<type_traits>
+#include<utility>
+#include<valarray>
+using namespace std;
+
+typedef long long i64;
+typedef unsigned long long u64;
+/*
+ * __builtin_ffs  __builtin_clz  __builtin_ctz __builtin_popcount  __builtin_parity
+ * sizeof CLOCKS_PER_SEC
+ */
+const double EPS = 1e-10;
+struct PT { 
+  double x, y; 
+  PT():x(0), y(0) {}
+  PT(int _x, int _y): x(_x), y(_y) {}
+  PT(const PT &p) : x(p.x), y(p.y)    {}
+  PT operator + (const PT &p)  const { return PT(x+p.x, y+p.y); }
+  PT operator - (const PT &p)  const { return PT(x-p.x, y-p.y); }
+  PT operator * (int c)     const { return PT(x*c,   y*c  ); }
+  PT operator / (int c)     const { return PT(x/c,   y/c  ); }
+  bool operator < (const PT &p)  const { return x != p.x ? x < p.x : y < p.y; }
+};
+
+double dot(PT p, PT q)     { return p.x*q.x+p.y*q.y; }
+double dist2(PT p, PT q)   { return dot(p-q,p-q); }
+double cross(PT p, PT q)   { return p.x*q.y-p.y*q.x; }
+ostream &operator<<(ostream &os, const PT &p) {
+  return os << "(" << p.x << "," << p.y << ")"; 
+}
+
+// rotate a point CCW or CW around the origin
+PT RotateCCW90(PT p)   { return PT(-p.y,p.x); }
+PT RotateCW90(PT p)    { return PT(p.y,-p.x); }
+PT RotateCCW(PT p, double t) { 
+  return PT(p.x*cos(t)-p.y*sin(t), p.x*sin(t)+p.y*cos(t)); 
+}
+
+// project point c onto line through a and b
+// assuming a != b
+PT ProjectPointLine(PT a, PT b, PT c) {
+  return a + (b-a)*dot(c-a, b-a)/dot(b-a, b-a);
+}
+
+// project point c onto line segment through a and b
+PT ProjectPointSegment(PT a, PT b, PT c) {
+  double r = dot(b-a,b-a);
+  if (fabs(r) < EPS) return a;
+  r = dot(c-a, b-a)/r;
+  if (r < 0) return a;
+  if (r > 1) return b;
+  return a + (b-a)*r;
+}
+
+// compute distance from c to segment between a and b
+double DistancePointSegment(PT a, PT b, PT c) {
+  return sqrt(dist2(c, ProjectPointSegment(a, b, c)));
+}
+
+// compute distance between point (x,y,z) and plane ax+by+cz=d
+double DistancePointPlane(double x, double y, double z,
+                          double a, double b, double c, double d)
+{
+  return fabs(a*x+b*y+c*z-d)/sqrt(a*a+b*b+c*c);
+}
+
+// determine if lines from a to b and c to d are parallel or collinear
+bool LinesParallel(PT a, PT b, PT c, PT d) { 
+  return fabs(cross(b-a, c-d)) < EPS; 
+}
+
+bool LinesCollinear(PT a, PT b, PT c, PT d) { 
+  return LinesParallel(a, b, c, d)
+      && fabs(cross(a-b, a-c)) < EPS
+      && fabs(cross(c-d, c-a)) < EPS; 
+}
+
+// determine if line segment from a to b intersects with 
+// line segment from c to d
+bool SegmentsIntersect(PT a, PT b, PT c, PT d) {
+  if (LinesCollinear(a, b, c, d)) {
+    if (dist2(a, c) < EPS || dist2(a, d) < EPS ||
+      dist2(b, c) < EPS || dist2(b, d) < EPS) return true;
+    if (dot(c-a, c-b) > 0 && dot(d-a, d-b) > 0 && dot(c-b, d-b) > 0)
+      return false;
+    return true;
+  }
+  if (cross(d-a, b-a) * cross(c-a, b-a) > 0) return false;
+  if (cross(a-c, d-c) * cross(b-c, d-c) > 0) return false;
+  return true;
+}
+
+// compute intersection of line passing through a and b
+// with line passing through c and d, assuming that unique
+// intersection exists; for segment intersection, check if
+// segments intersect first
+PT ComputeLineIntersection(PT a, PT b, PT c, PT d) {
+  b=b-a; d=c-d; c=c-a;
+  assert(dot(b, b) > EPS && dot(d, d) > EPS);
+  return a + b*cross(c, d)/cross(b, d);
+}
+
+// compute center of circle given three points
+PT ComputeCircleCenter(PT a, PT b, PT c) {
+  b=(a+b)/2;
+  c=(a+c)/2;
+  return ComputeLineIntersection(b, b+RotateCW90(a-b), c, c+RotateCW90(a-c));
+}
+
+
+// compute intersection of line through points a and b with
+// circle centered at c with radius r > 0
+vector<PT> CircleLineIntersection(PT a, PT b, PT c, double r) {
+  vector<PT> ret;
+  b = b-a;
+  a = a-c;
+  double A = dot(b, b);
+  double B = dot(a, b);
+  double C = dot(a, a) - r*r;
+  double D = B*B - A*C;
+  if (D < -EPS) return ret;
+  ret.push_back(c+a+b*(-B+sqrt(D+EPS))/A);
+  if (D > EPS)
+    ret.push_back(c+a+b*(-B-sqrt(D))/A);
+  return ret;
+}
+
+// compute intersection of circle centered at a with radius r
+// with circle centered at b with radius R
+vector<PT> CircleCircleIntersection(PT a, PT b, double r, double R) {
+  vector<PT> ret;
+  double d = sqrt(dist2(a, b));
+  if (d > r+R || d+min(r, R) < max(r, R)) return ret;
+  double x = (d*d-R*R+r*r)/(2*d);
+  double y = sqrt(r*r-x*x);
+  PT v = (b-a)/d;
+  ret.push_back(a+v*x + RotateCCW90(v)*y);
+  if (y > 0)
+    ret.push_back(a+v*x - RotateCCW90(v)*y);
+  return ret;
+}
+
+// This code computes the area or centroid of a (possibly nonconvex)
+// polygon, assuming that the coordinates are listed in a clockwise or
+// counterclockwise fashion.  Note that the centroid is often known as
+// the "center of gravity" or "center of mass".
+double ComputeSignedArea(const vector<PT> &p) {
+  double area = 0;
+  for(size_t i = 0; i < p.size(); i++) {
+    int j = (i+1) % p.size();
+    area += p[i].x*p[j].y - p[j].x*p[i].y;
+  }
+  return area / 2.0;
+}
+
+double ComputeArea(const vector<PT> &p) {
+  return fabs(ComputeSignedArea(p));
+}
+
+PT ComputeCentroid(const vector<PT> &p) {
+  PT c(0,0);
+  double scale = 6.0 * ComputeSignedArea(p);
+  for (size_t i = 0; i < p.size(); i++){
+    int j = (i+1) % p.size();
+    c = c + (p[i]+p[j])*(p[i].x*p[j].y - p[j].x*p[i].y);
+  }
+  return c / scale;
+}
+
+// tests whether or not a given polygon (in CW or CCW order) is simple
+bool IsSimple(const vector<PT> &p) {
+  for (size_t i = 0; i < p.size(); i++) {
+    for (size_t k = i+1; k < p.size(); k++) {
+      size_t j = (i+1) % p.size();
+      size_t l = (k+1) % p.size();
+      if (i == l || j == k) continue;
+      if (SegmentsIntersect(p[i], p[j], p[k], p[l])) 
+        return false;
+    }
+  }
+  return true;
+}
+const int MAX_S = 250000;
+const int MAX_R = 20;
+const int MAX_W = 10;
+const int MAX_P = 10000;
+int s, r, w, p;
+//struct Pos{
+  //int x, y;
+//};
+typedef PT Pos;
+struct Seg{
+  Pos from, to;
+  Seg():from(), to(){};
+};
+Pos sensors[MAX_S];
+int output[MAX_S];
+Seg lines[MAX_W];
+Pos products[MAX_P];
+void input(){
+  scanf("%d%d%d%d", &s, &r, &w, &p);
+  for (int i = 0; i < s; i++) scanf("%lf%lf", &sensors[i].x, &sensors[i].y);
+  for (int i = 0; i < w; i++) scanf("%lf%lf%lf%lf", &lines[i].from.x, &lines[i].from.y, &lines[i].to.x, &lines[i].to.y);
+  for (int i = 0; i < p; i++) scanf("%lf%lf", &products[i].x, &products[i].y);
+}
+
+bool is_intersect(Pos &a1, Pos &a2, Pos &b1, Pos &b2){
+  return SegmentsIntersect(a1, a2, b1, b2); 
+  //return false;
+}
+bool valid(Pos &a, Pos &b){
+  int dist2 = (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+  int R = r; 
+  for (int i = 0; i < w; i++){ 
+    if (R <= 0 || dist2 > R * R) return false;
+    if (is_intersect(a, b, lines[i].from, lines[i].to)){
+      R--;
+    }
+  }
+  return R > 0 && dist2 <= R * R;
+}
+void solve(){
+  sort(sensors, sensors + s);
+  for (int j = 0; j < p; j++){
+    int from = lower_bound(sensors, sensors + s, Pos(products[j].x - r - 1, -10000)) - sensors; 
+    int to = upper_bound(sensors, sensors + s, Pos(products[j].x + r + 1, 10000)) - sensors; 
+    int len = 0;
+    for (int i = from; i < to; i++){
+    //for (int i = 0; i < s; i++){
+      //if (abs(sensors[i].x - products[j].x) > r) continue;
+      //if (abs(sensors[i].y - products[j].y) > r) continue;
+      if (valid(sensors[i], products[j])) output[len++] = i;
+    } 
+    printf("%d", len);
+    for (int i = 0; i < len; i++){
+      Pos &P = sensors[output[i]];
+      printf(" (%.lf,%.lf)", P.x, P.y);
+    }
+    printf("\n");
+  }
+}
+int TestNum;
+int main(){
+  scanf("%d", &TestNum);
+  while (TestNum--) {
+    input();
+    solve();
+  }
+}
+
