65e68e96d459582cce6452075c50bf67d62a08ef
darrenhp <darrenhp@gmail.com>
2014-05-07 00:43:29 +0800

    b

--- programming-challenges/ch14/uva_849.cpp
+++ programming-challenges/ch14/uva_849.cpp
@@ -77,7 +77,7 @@ using namespace std;
 typedef long long i64;
 typedef unsigned long long u64;
 const double PI = acos(-1);
-const double EPS = 1e-6;
+const double EPS = 1e-2;
 /*
  * __builtin_ffs  __builtin_clz  __builtin_ctz __builtin_popcount  __builtin_parity
  * sizeof CLOCKS_PER_SEC
@@ -139,49 +139,82 @@ void gao3(){
     }
     double d32_ = d2 / sin(radiusFromAngle(a032)) * sin(radiusFromAngle(360 - a32)); 
 
-    d3 = d2 / sin(radiusFromAngle(a032)) * sin(radiusFromAngle(180 - a021)); 
-    a3 = angleAdjust(a32 + a2); 
-    //printf("bin: %lf %lf, %lf\n", lo, hi, mid);
     //printf("%lf\n", a021);
     //printf("%lf %lf\n", a032, a32);
     //printf("%lf %lf\n", d32_, d32);
     if (d32_ > d32) lo = mid;
     else hi = mid;
+    if (fabs(d32_ - d32) < EPS) {
+      d3 = d2 / sin(radiusFromAngle(a032)) * sin(radiusFromAngle(180 - a021)); 
+      a3 = angleAdjust(a32 + a2); 
+    }
+    d3 = d2 / sin(radiusFromAngle(a032)) * sin(radiusFromAngle(180 - a021)); 
+    a3 = angleAdjust(a32 + a2); 
+    //printf("bin: %lf %lf, %lf\n", lo, hi, mid);
+    //printf("bin: %lf %lf, %lf\n", lo, hi, mid);
   }
   //printf("%.02lf %.02lf\n", lo, hi); 
   printf("%.02lf %.02lf\n", a3, d3); 
 }
+bool Pcmp(pair<double, double> a, pair<double, double> b){
+  if (a.first != b.first) return a.first < b.first;
+  return a.second > b.second;
+}
+bool Pequ(pair<double, double> a, pair<double, double> b){
+  return fabs(a.first - b.first) < EPS && fabs(a.second - b.second) < EPS;
+}
 void gao4(){
+  vector<pair<double, double> > ans;
   for (double a21d = 0; a21d <= 360; a21d += 360){
-    for (double tt  = 0; tt <= 360; tt += 360){
+    for (double tt = 0; tt <= 360; tt += 360){
       double d21 = getByConsines(d1, d2, a21);
       double lo = d21 / (a21d + a21) * (tt + 0), hi = d21 / (a21d + a21) * (tt + 180 - a21); 
       d3 = -1;
       while (fabs(lo - hi) > 1e-10) {
         double mid = (lo + hi) / 2;
+      //for (double mid = lo; mid < hi; mid += 1e-6) {
+        //cout << mid << endl;
         double d32 = mid;
         double a32 = angleAdjust(d32 / d21 * (a21 + a21d));
         double a021 = getAngleByConsines(d21, d2, d1);
         double a032 = a021 - a32; 
-        if (a032 < 0){
-          //assert(0);
-          lo = mid;
-          continue;
-        }
-        double d32_ = d2 / sin(radiusFromAngle(a032)) * sin(radiusFromAngle(a32)); 
 
-        d3 = d2 / sin(radiusFromAngle(a032)) * sin(radiusFromAngle(180 - a021)); 
-        a3 = angleAdjust(a32 + a2); 
+        double dd3 = d32 / sin(radiusFromAngle(a32)) * sin(radiusFromAngle(180 - a021));
+        double dd3_ = d2 / sin(radiusFromAngle(a032)) * sin(radiusFromAngle(180 - a021)); 
+
+        //if (a032 < 0){
+          //printf("%lf\n", a032);
+          //lo = mid;
+          //continue;
+        //}
+        //double d32_ = d2 / sin(radiusFromAngle(a032)) * sin(radiusFromAngle(a32)); 
+
         //printf("bin: %lf %lf, %lf\n", lo, hi, mid);
         //printf("%lf\n", a021);
         //printf("%lf %lf\n", a032, a32);
         //printf("%lf %lf\n", d32_, d32);
-        if (d32_ > d32) lo = mid;
+        //if (d32_ > d32) lo = mid;
+        //else hi = mid;
+        if (dd3 > dd3_) lo = mid;
         else hi = mid;
+        if (fabs(dd3 - dd3_) < 1e-2){
+          //d3 = d2 / sin(radiusFromAngle(a032)) * sin(radiusFromAngle(180 - a021)); 
+          a3 = angleAdjust(a32 + a2); 
+          d3 = dd3 ;
+          ans.push_back(make_pair(a3, d3));
+          break;
+        }
       }
-      printf("%.02lf %.02lf\n", a3, d3); 
     }
   }
+  sort(ans.begin(), ans.end());
+  ans.erase(unique(ans.begin(), ans.end(), Pequ), ans.end());
+  for (size_t i = 0; i < ans.size(); i++){
+    if (i != 0) printf(" ");
+    printf("%.02lf %.02lf", ans[i].first, ans[i].second);
+  }
+  printf("\n");
+  //printf(" gao4\n");
   //printf("gao4\n");
 }
 int TestNum;
