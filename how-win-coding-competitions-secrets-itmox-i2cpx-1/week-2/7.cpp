7ca3ed3ca81a1bbbc78b9ac09f761c2e05468578
darrenhp <darrenhp@gmail.com>
2017-10-25 20:36:09 +0800

    w2

--- how-win-coding-competitions-secrets-itmox-i2cpx-1/week-2/7.cpp
+++ how-win-coding-competitions-secrets-itmox-i2cpx-1/week-2/7.cpp
@@ -5,25 +5,28 @@
 #include<string>
 #include<numeric>
 #include<map>
+#include<functional>
 using namespace std;
 int main() {
   freopen("input.txt", "r", stdin);
   freopen("output.txt", "w", stdout);
   int N; scanf("%d", &N);
-  map<int, int> M;
+  priority_queue<int, vector<int>, greater<int>> Q;
   int h = 0;
   while (N--) {
     int a; scanf("%d", &a);
     if (a > 0) {
-      if (h != 0) {
-        M[h]++; 
-      }
-      h = 1;
+      Q.push(1);
     } else {
-      h++;
+      if (Q.size() == 0) Q.push(1);
+      else {
+        int x = Q.top(); Q.pop();
+        Q.push(x + 1);
+      }
     }
   }
-  if (h != 0) M[h]++;
+  map<int, int> M;
+  while (Q.size()) M[Q.top()]++, Q.pop();
   cout << M.size() << endl;
   for (auto it: M) {
     cout << it.first << " " << it.second << endl;
